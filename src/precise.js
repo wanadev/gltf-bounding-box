68c2f970a3003602aef9fb9450dfc70702c42f72
Jean-Baptiste Ghoul <jbghoul@wanadev.fr>
2019-03-19 09:58:59 +0100

    2 spaces for tabs

--- src/precise.js
+++ src/precise.js
@@ -1,13 +1,13 @@
 const precise = {
     
 	/**
-     * @public
-     * @param {Number} number
-     * @param {String|Number} precision the precision to round up the number
-     * @return {Number} the rounded number
-	* 
-	* If precision is not 0, the number is rounded using ceil to avoid having a bbox smaller than the actual object.
-     */
+   * @public
+   * @param {Number} number
+   * @param {String|Number} precision the precision to round up the number
+   * @return {Number} the rounded number
+	 * 
+	 * If precision is not 0, the number is rounded using ceil to avoid having a bbox smaller than the actual object.
+   */
 	round(number, precision) {
 		if (precision === 0) {
 			return Math.round(number);
